f36afe128c545e0525daa1f7bf1458c7e26671d1
Sabbir <sabbir.ul.alam.sabbir@gmail.com>
2025-06-21 21:31:48 +0600

    Add scroll test

---
+++ tests/test_home_page.py
@@ -0,0 +1,24 @@
+import time
+
+from click import pause
+from playwright.sync_api import  Page
+
+from page_objects.home_page import HomePage
+
+
+def test_verify_scrollup_functionality_using_arrow_icon(page: Page):
+    homepage = HomePage(page)
+    homepage.visit()
+    assert homepage.is_loaded()
+
+    locator = page.locator('h2').get_by_text('Subscription')
+    locator.scroll_into_view_if_needed()
+    assert locator.is_visible()
+
+    page.locator('#scrollUp').click()
+    text = page.get_by_text('Full-Fledged practice website for Automation Engineers').first
+    text.scroll_into_view_if_needed()
+    bounding_box = text.bounding_box()
+    assert bounding_box is not None, "Element not found on the page"
+    assert text.is_visible()
+
